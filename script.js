5bf0b63d4070c0bfc2d4a6fb5333bec1e658dbd0
Diana Carolina Russi Posada <diana.russiposada@gmail.com>
2024-03-02 18:41:33 -0500

    funcion desencriptar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const mensajeParaEncriptar = document.querySelector('.mensajeParaEncriptar');
-const mensajeEncriptado = document.querySelector('.mensajeEncriptado');
+const mensajeParaEncriptarDesencriptar = document.querySelector('.mensajeParaEncriptarDesencriptar');
+const mensajeEncriptadoDesencriptado = document.querySelector('.mensajeEncriptadoDesencriptado');
 
 //Llaves de encriptacion
 /* La letra "e" es convertida para "enter"
@@ -9,11 +9,10 @@ const mensajeEncriptado = document.querySelector('.mensajeEncriptado');
  * La letra "u" es convertida para "ufat" */
 
 function btnEncriptar(){
-    const textoEncriptado = encriptar(mensajeParaEncriptar.value);
-    mensajeEncriptado.value = textoEncriptado;
-    mensajeParaEncriptar.value = "";
-    mensajeEncriptado.style.backgroundImage = 'none';
-
+    const textoEncriptado = encriptar(mensajeParaEncriptarDesencriptar.value);
+    mensajeEncriptadoDesencriptado.value = textoEncriptado;
+    mensajeParaEncriptarDesencriptar.value = "";
+    mensajeEncriptadoDesencriptado.style.backgroundImage = 'none';
 }
 
 function encriptar(stringEncriptado){
@@ -23,6 +22,23 @@ function encriptar(stringEncriptado){
         if(stringEncriptado.includes(llavesEncriptacion[i][0])){
             stringEncriptado = stringEncriptado.replaceAll(llavesEncriptacion[i][0], llavesEncriptacion[i][1]);
         }
-        return stringEncriptado;
     }
+    return stringEncriptado;
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(mensajeParaEncriptarDesencriptar.value);
+    mensajeEncriptadoDesencriptado.value = textoDesencriptado;
+    mensajeParaEncriptarDesencriptar.value = "";
+}
+
+function desencriptar(stringDesencriptado){
+    let llavesEncriptacion =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+    for (let i = 0; i < llavesEncriptacion.length; i++) {
+        if(stringDesencriptado.includes(llavesEncriptacion[i][1])){
+            stringDesencriptado = stringDesencriptado.replaceAll(llavesEncriptacion[i][1], llavesEncriptacion[i][0]);
+        }
+    }
+    return stringDesencriptado;
 }
